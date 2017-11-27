2a4361f92747f9d2f058280e66000eba90bcd7b1
paljen <31762847+paljen@users.noreply.github.com>
2017-11-27 09:45:25 +0100

    Update Template_ComponentRunbook.ps1

--- Template_ComponentRunbook.ps1
+++ Template_ComponentRunbook.ps1
@@ -10,37 +10,38 @@
     •	Flow specific runbooks - Tag: Controller
     •	Integration runbooks combined with webhooks - Tag: Interface
 
-.PARAMETER ParameterName
-    The description of a parameter. (Add PARAMETER keyword for each parameter)
-    
+.PARAMETER  <ParameterName>
+	The description of a parameter. (Add .PARAMETER keyword for each parameter)
+
 .OUTPUTS
     [Object]
 
 .NOTES
-    Version : 2.0
-    Author  : Admin-PJE	
-    Note    : {[Major.Minor]} - {Date}, {Username}, {Description}
-    Note    : 1.0 - 21-09-2017, Admin-PJE, Initial runbook development
-    Note    : 1.1 - 21-09-2017, Admin-PJE, Set ErrorActionPreference to STOP
-    Note    : 2.0 - 21-09-2017, Admin-PJE, Added Verbose logging functionallity
+    Version  : 1.0
+    Template : Component
+    tVersion : 2.2
+    Author   : {Username}		
+    Note     : {[Major.Minor]} - {Date}, {Username}, {Description}
+    Note     : 1.0 - 21-02-2017, Admin-PJE, Initial template development
+    Note     : 2.0 - 28-09-2017, Admin-PJE, Logging implemented
+    Note     : 2.1 - 21-09-2017, Admin-PJE, Added verbose output and some minor logging details
+    Note     : 2.2 - 21-09-2017, Admin-PJE, Added minor logging details
 #>
+
+[CmdletBinding()]
 
 Param(
 
     <# Adjust Parmameters to what is needed
     [Parameter(Mandatory=$true)]
-    [String]$Name,
-
-    [Parameter(Mandatory=$true)]
-    [String]$SamAccountName#>
+    [String]$Name#>
 
 )
 
 $ErrorActionPreference = "Stop"
 
 <# To enable verbose stream set `$VerbosePreference to Continue, Verbose should also be 
-   switched "On" on the runbook in Azure portal. Use Write-Verbose 
-   in the runbook to write to verbose stream#>
+   switched "On" on the runbook in Azure portal#>
 $VerbosePreference = "SilentlyContinue"
 
 # Provide the runbook with the same name as in Azure, this variable is used mainly for tracking
@@ -48,22 +49,23 @@ $RunbookName = ""
 
 try
 {
-    #region Initialize Log
-    $repo = Get-AutomationVariable -Name Repository
-    . $repo\Classes\Log.ps1
+    if ([String]::IsNullOrEmpty($RunbookName)) 
+    {Throw "`$RunbookName variable not initialized"}
 
-    # Initialize Log
-    [Array]$instance = [RunbookLog]::New()
+    $StartTime = Get-date
 
-    # Clone Logging Array to ArrayList to utalize array methods
-    [System.Collections.ArrayList]$Log = $instance.Clone()
+    # Initialize workspace and repositories
+    $ws = Get-AutomationVariable -Name Workspace
+    . $ws\AzureAutomation\Repository\Classes\Log.ps1
 
-    # Clear Cloned array
-    $Log.clear()
-    #endregion
+    # Initialize Logging
+    $FilePath = "$ws\AzureAutomation\Logs\$RunbookName.Log"
 
-    # Initialize trace output stream, if the runbook is run in Azure the computername will return CLIENT
-    $Log.Add($instance.WriteLogEntry($RunbookName,"Running on ($env:COMPUTERNAME)")) | Out-Null
+    # If the log file is larger then 2 MB rename the the file, a new log file will be gererated
+    Get-Item -Path $FilePath -ErrorAction SilentlyContinue | ? {$_.length / 1KB -gt 2048} | Rename-Item -NewName $RunbookName-$((get-date).tostring("MMddyyyyHHmm")).log
+
+    [RunbookLog]$rbLog = [RunbookLog]::new($FilePath,$RunbookName)
+    $rbLog.WriteLogEntry($RunbookName, "Runbook started")
 
     # Optional - Connect to Azure Resource Manager, ignore if this is called from an Control runbook 
     # Where connection already has been initialized with the variable `$conn
@@ -73,29 +75,23 @@ try
     catch{
         $conn = .\Connect-AzureRMAutomation.ps1
         
-        # Recieve log from sub runbook
-        foreach ($i in $conn.Trace){
-            $Log.Add($instance.WriteLogEntry($i.RunbookName,$i.Message)) | Out-Null
-        }
+        $rbLog.WriteLogEntry($conn)
 
         if($conn.status -ne "Success"){
-            Throw "Error - Connecting to Azure failed"
+            Throw "Connecting to Azure failed"
         } 
     }
            
-    # Import Modules and setup implicit remoting, use component runbooks to setup implicit remoting
+    # Import Modules and setup implicit remoting, use core runbooks to setup implicit remoting
     $modules = @()
     $modules += .\Connect-MSOnline.ps1
     $modules += .\Connect-ExchangeOnline.ps1
 
-    # Recieve log from sub runbook
-    foreach ($i in $modules.trace){
-        $Log.Add($instance.WriteLogEntry($i.RunbookName,$i.Message)) | Out-Null
-    }
+    $rbLog.WriteLogEntry($modules)
 
-    # Make sure that the modules were imported for the runspace
+    # Make sure that the modules were imported
     if($modules[0].ObjectCount -lt 1 -or $modules[1].ObjectCount -lt 1){
-        Throw "Error - One or more modules was not imported"
+        Throw "One or more modules failed importing"
     }
 
     <#
@@ -108,15 +104,14 @@ try
                         'ObjectCount' = 1
     }
 }
-
 catch
 {
     # Add to trace on what line the error occured and the exception message
-    $excep = $(if($_.Exception.Message.Contains("`"")){$_.Exception.Message.Replace("`"","'")}else{$_.Exception.Message})
-    $Log.Add($instance.WriteLogEntry($RunbookName,"Exception Caught at line $($_.InvocationInfo.ScriptLineNumber), $excep")) | Out-Null
+    $excep = $(if($error[0].Exception -contains ("`"")){$error[0].Exception -Replace ("`"","'")}else{$error[0].Exception})
+    $rbLog.WriteLogEntry($RunbookName, "Failed - Exception Caught at line $($error[0].InvocationInfo.ScriptLineNumber), $excep")
 
     # Return values used for further processing, add properties if needed
-    $props = [Ordered]@{'Status' = $status
+    $props = [Ordered]@{'Status' = "Failed"
                         'Message' = $excep
                         'ObjectCount' = 0
     }
@@ -125,23 +120,16 @@ catch
 }
 finally
 {
-    # Remove session if needed, change if needed
-    try{
-        Remove-PSSession -name $($modules[1].Connect.Session.Name)
-        $Log.Add($Instance.WriteLogEntry($RunbookName,"Removing session $($modules[1].Connect.Session.Name)")) | Out-Null
-    }
-    catch{
-        $Log.Add($Instance.WriteLogEntry($RunbookName,"Session does not exist $($modules[1].Connect.Session.Name)")) | Out-Null
-    }
+    Remove-PSSession -name $($modules[1].Connect.Session.Name) -ErrorAction SilentlyContinue
 
-    $props.Add('Trace',$trace)
+    $rbLog.WriteLogEntry($RunbookName,"Runbook finished - total runtime: $((([DateTime]::Now) - $StartTime).TotalSeconds) Seconds")
+    $rbLog.Log.GetEnumerator() | foreach { Write-Verbose "$($_.RunbookName), $($_.Message), $($_.TimeStamp)" }
+
+    # Finalize output object
+    $props.Add('Trace',$rbLog.Log)
     $props.Add('RunbookName',$RunbookName)
 
     $out = New-Object -TypeName PSObject -Property $props
 
-    # optional, use Send-Email to send email or sms notifications, for sms use EmailAddressTo file with +45xxxxxxxx@sms.ecco.local
-    # Body parameter has to be an object
-    $email = .\Send-Email.ps1 -EmailAddressTO $(Get-AutomationVariable -Name "Email-PJE") -Subject "Daily Report $RunbookName" -Body $Out -AsHtml
-
     Write-Output $out
-}  
+}
