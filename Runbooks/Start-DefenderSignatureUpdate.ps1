2dc0c6c51caf3daf0678ad8ef7aac99a2f8eeb31
paljen <31762847+paljen@users.noreply.github.com>
2017-11-28 13:38:07 +0100

    Create Start-DefenderSignatureUpdate.ps1

---
+++ Runbooks/Start-DefenderSignatureUpdate.ps1
@@ -0,0 +1,74 @@
+ 
+<#
+.DESCRIPTION
+    A brief description on what is going on in the runbook
+
+.INPUTS
+    NA
+
+.OUTPUTS
+    NA
+
+.NOTES
+    Version:        1.0.0
+    Author:			
+    Creation Date:	
+    Purpose/Change:	Initial runbook development
+#>
+
+Param(
+    [String]$ServerName
+)
+
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "SilentlyContinue" #Continue = enable
+$RunbookName = "Start-DefenderQuickScan"
+
+try
+{
+    # Initialize trace output stream
+    $trace = ""
+
+    # Logic goes here
+    ##
+    $trace += "$([DateTime]::Now.ToString())`tStarting a QuickScan on server: $($ServerName)`n"
+    ##
+    
+    Invoke-Command -ComputerName $ServerName -ScriptBlock {import-module Defender; Start-MpScan -ScanType QuickScan}
+    
+    $trace += "$([DateTime]::Now.ToString())`tFinished a QuickScan on server: $($ServerName)`n"
+       
+    # Return values used for further processing
+    $props = @{'Status' = "Success"
+               'Message' = "Workflow Finished Successfully"
+               'ObjectCount' = 1}
+}
+
+catch
+{
+    # Add to trace on what line the error occured and the exception message
+    $excep = $(if($_.Exception.Message.Contains("`"")){$_.Exception.Message.Replace("`"","'")}else{$_.Exception.Message})
+    $trace += "$([DateTime]::Now.ToString())`tException Caught at line $($_.InvocationInfo.ScriptLineNumber), $excep`n"
+
+    # If you throw the error 
+    if($_.Exception.WasThrownFromThrowStatement)
+    {$status = "failed"}
+    else
+    {$status = "warning"}
+
+    # Return values used for further processing
+    $props = @{'Status' = $status
+               'Message' = "Error"
+               'ObjectCount' = 0}
+    
+    Write-Error $status
+}
+finally
+{
+    $props.Add('Trace',$trace)
+    $props.Add('RunbookName',$RunbookName)
+
+    $out = New-Object -TypeName PSObject -Property $props
+
+    Write-Output $out
+} 
